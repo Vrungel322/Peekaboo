220ac8b9fbb109b88451cdc4993bdeb248c12294
vladislavGutov <vladislav.gutov@gmail.com>
2016-06-27 13:21:38 +0300

    implement dummy signin signup

--- src/main/java/com/peekaboo/controller/SignController.java
+++ src/main/java/com/peekaboo/controller/SignController.java
@@ -1,34 +1,54 @@
 package com.peekaboo.controller;
 
+import com.peekaboo.controller.helper.SigninRequestEntity;
+import com.peekaboo.controller.helper.SignupRequestEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
 
 @RestController
 public class SignController {
 
     @RequestMapping(value = "/signin", method = RequestMethod.POST)
-    public ResponseEntity<SignResponse> signin(@RequestParam String username, @RequestParam String password) {
-        if (username.equals(password)) {
-            return new ResponseEntity<SignResponse>(HttpStatus.NOT_FOUND);
+    public ResponseEntity signin(@Valid SigninRequestEntity requestEntity, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return new ResponseEntity(errors.getAllErrors().toArray(), HttpStatus.BAD_REQUEST);
+        }
+
+        if (requestEntity.getUsername().equals(requestEntity.getPassword())) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
             SignResponse response = new SignResponse();
             response.setId("1");
-            ResponseEntity<SignResponse> responseEntity = new ResponseEntity<SignResponse>(response, HttpStatus.OK);
+            ResponseEntity responseEntity = new ResponseEntity(response, HttpStatus.OK);
             return responseEntity;
         }
     }
 
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public ResponseEntity signup(@Valid SignupRequestEntity requestEntity, Errors errors) {
+        if (errors.hasErrors()) {
+            return new ResponseEntity(errors.getAllErrors().toArray(), HttpStatus.BAD_REQUEST);
+        }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public SignResponse hello() {
-        SignResponse sr = new SignResponse();
-        sr.setId("hello world");
-        return sr;
+        //temporal solution
+        if (requestEntity.getUsername().equals(requestEntity.getPassword())) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        SignResponse response = new SignResponse();
+        response.setId("1");
+
+        return new ResponseEntity(response, HttpStatus.OK);
+
     }
+
+
 
     private class SignResponse {
         private String id;
--- src/main/java/com/peekaboo/controller/helper/SignupRequestEntity.java
+++ src/main/java/com/peekaboo/controller/helper/SignupRequestEntity.java
@@ -1,4 +1,50 @@
 package com.peekaboo.controller.helper;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public class SignupRequestEntity {
+
+    @Email
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    @Length(min = 6, max = 25)
+    private String username;
+
+    @NotNull
+    @Length(min = 6)
+    @Pattern(regexp = "[\\d|\\w|_]+")
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }

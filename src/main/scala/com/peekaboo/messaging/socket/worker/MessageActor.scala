1e6e2e3bb5003c97320b7a95be276fa23a7072fc
Oleksii <romanko1993@gmail.com>
2016-08-23 20:20:57 +0300

    Rejected commit: Default

--- src/main/scala/com/peekaboo/messaging/socket/worker/MessageActor.scala
+++ src/main/scala/com/peekaboo/messaging/socket/worker/MessageActor.scala
@@ -1,12 +1,14 @@
 package com.peekaboo.messaging.socket.worker
 
+import java.io.File
+import java.nio.file.{Path, Paths}
 import java.util.UUID
 
 import akka.actor.Actor
 import com.peekaboo.messaging.socket.middleware.BinaryMessageInterceptor
+import com.peekaboo.transformation.{AudioToTextInterface, AudioToTextWatson, SpeechRecognitionOptions}
 import org.apache.logging.log4j.LogManager
 import org.springframework.web.socket.{BinaryMessage, WebSocketSession}
-
 class MessageActor(private val socket: WebSocketSession) extends Actor {
 
 
@@ -29,12 +31,33 @@ class MessageActor(private val socket: WebSocketSession) extends Actor {
   }
 
   def receive = {
-    case (msg: Send, id: String) =>
+    case (msg: Send,destinationId: String) =>
+        val converter: AudioToTextInterface = new AudioToTextWatson
+        val rootPath: String = System.getProperty("catalina.home")
+        val rootDir = new File(rootPath + File.separator + "tmp")
+//        logger.error(new String(msg.getBody, "UTF-8"))
+        val filename = new String(msg.getBody, "UTF-8")
+        val file: Path = java.nio.file.Paths.get(rootDir.getAbsolutePath,destinationId, filename)
+      logger.debug("Target file:"+file)
+
+      try {
+        val textConverted = converter.RunServiceWithDefaults(file.toFile)
+        logger.error(textConverted)
         //nice place to add conversion
-      //have to check user's state
-      //and than if needed perform some actions
-      val action = msg.toMessage(id)
-      sendMessage(action)
+        //have to check user's state
+        //and than if needed perform some actions
+        //val audiomsg = new Send(textConverted.getBytes("UTF-8"), msg.parameters)
+        //val action = audiomsg.toMessage(msg.getSender)
+        val m=new SendText(textConverted,msg.parameters)
+
+        val action = m.toMessage(destinationId)
+        sendMessage(action)
+      }
+      catch {
+        case ex: Exception =>
+          logger.error(ex.getMessage)
+      }
+
     case a =>
       logger.debug("Message received but it cannot be resolved")
       logger.debug(a)
